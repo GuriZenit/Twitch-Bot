78134100bbef1f78be772ff0883d20282fd055c0
GuriZenit <israelreis17@gmail.com>
2021-06-28 20:16:40 -0300

    Bug Fix

--- commands/sociais.js
+++ commands/sociais.js
@@ -1,7 +1,7 @@
 module.exports.run = async (channel, tags, msg, self, client) => {
-  const twitter = 'https:twitter.com/gurizenit/'
+  const twitter = 'https://twitter.com/gurizenit'
   const instagram = 'https://instagram.com/gurizenit/'
-  const myAnimeList = 'https://myanimelist.net/gurizenit/'
+  const myAnimeList = 'https://myanimelist.net/profile/gurizenit/'
   client.say(channel,`
   Twitter: ${twitter} 
   Instagram: ${instagram} 

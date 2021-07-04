0b95810a904731caa6178115b86f01292b30f7b9
GuriZenit <israelreis17@gmail.com>
2021-07-04 15:22:07 -0300

    test

--- commands/hi.js
+++ commands/hi.js
@@ -1,3 +1,3 @@
 module.exports.run = async (channel, tags, msg, self, client) => {
-  client.say(channel, `Olá ${tags.username}!! how are you doing`) 
+  client.say(channel, `Olá ${tags.username}!! how are you doing?`) 
 }
--- commands/sociais.js
+++ commands/sociais.js
@@ -2,8 +2,10 @@ module.exports.run = async (channel, tags, msg, self, client) => {
   const twitter = 'https://twitter.com/gurizenit'
   const instagram = 'https://instagram.com/gurizenit/'
   const myAnimeList = 'https://myanimelist.net/profile/gurizenit/'
+  const webSite = 'https://gurizenit.tk'
   client.say(channel,`
   Twitter: ${twitter} 
   Instagram: ${instagram} 
-  MyAnimeList: ${myAnimeList}`)
+  MyAnimeList: ${myAnimeList}
+  Website: ${webSite}`)
 }

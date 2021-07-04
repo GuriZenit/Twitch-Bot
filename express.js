521e4bdba2e42064c5d5e55837bab0e1bc8489ae
GuriZenit <77638636+GuriZenit@users.noreply.github.com>
2021-07-04 15:29:02 -0300

    Update express.js

--- express.js
+++ express.js
@@ -1,10 +1,31 @@
-{
-  const express = require('express')
-  const app = express()
-  app.get('/', (req, res) => {
-    res.send(`Server Online`)
-    });
-    app.listen(3000, () => {
-      console.log('server started')
+import { readLines } from "https://deno.land/std@0.90.0/io/bufio.ts"
+import { opine, text } from "https://deno.land/x/opine@1.2.0/mod.ts"
+
+const app = opine()
+app.use(text())
+
+console.log("server running")
+
+app.post("/refresh", async (req, res) => {
+    console.log("repl.deploy" + req.body + req.headers.get("Signature"))
+
+    const result: {
+        body: string
+        status: number
+    } = JSON.parse((await getStdinLine())!)
+
+    await res.setStatus(result.status).end(result.body)
+    console.log("repl.deploy-success")
 })
+
+app.get("/", async (_, res) => {
+    res.end("OK")
+})
+
+async function getStdinLine() {
+    for await (const line of readLines(Deno.stdin)) {
+        return line
+    }
 }
+
+app.listen(8000)

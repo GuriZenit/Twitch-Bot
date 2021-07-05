1072a17eb9db1b789b43da6e4957988213b01d75
GuriZenit <israelreis17@gmail.com>
2021-07-04 23:03:12 -0300

    Resolvido caso não tenha algo na mensagem

--- commands/add.js
+++ commands/add.js
@@ -1,8 +1,14 @@
 module.exports.run = async (channel, tags, msg, self, client, args, db) => {
+  const defaults = ['add', 'remove', 'commands']
+  
   let comando = args.shift().toLowerCase()
   
-  client.say(channel, `Comando ${comando} adicionado!`)
+  if(!comando) return;
+  
+  if(defaults.indexOf(comando) >= 0){
+    return client.say(channel, `Não é possível adicionar esse comando!!`)
+  }
   
   await db.set(comando, args.join(" "))
-  
+  client.say(channel, `Comando ${comando} adicionado!`)
 }
--- commands/remove.js
+++ commands/remove.js
@@ -1,14 +1,18 @@
 module.exports.run = async (channel, tags, msg, self, client, args, db) => {
   const defaults = ['add', 'remove', 'commands']
-  
+
   let comando = args.shift().toLowerCase()
-  if(defaults.indexOf(comando)){
+  
+  if(!comando) return;
+  
+  if (defaults.indexOf(comando) >= 0) {
+    console.log(defaults.indexOf(comando))
     return client.say(channel, "Não é possível remover esse comando!")
   }
- if(await db.get(comando)){
-  await db.delete(comando)
-  client.say(channel, `Comando ${comando} foi apagado!`)
- } else {
-   client.say(channel, `Comando ${comando} não existe!`)
- }
+  if (await db.get(comando)) {
+    await db.delete(comando)
+    client.say(channel, `Comando ${comando} foi apagado!`)
+  } else {
+    client.say(channel, `Comando ${comando} não existe!`)
+  }
 }

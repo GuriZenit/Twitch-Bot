ff087167455b8bcf46fec2055f7785c6fadb3c1e
GuriZenit <israelreis17@gmail.com>
2021-07-05 04:08:33 -0300

    Bug fix?

--- commands/remove.js
+++ commands/remove.js
@@ -1,7 +1,7 @@
 module.exports.run = async (channel, tags, msg, self, client, args, db) => {
   const defaults = ['add', 'remove', 'commands']
   
-  if(!args[1]) return client.say(channel, "Comando não pode ficar vazio"); 
+  if(!args[0]) return client.say(channel, "Comando não pode ficar vazio"); 
   
   let comando = args.shift().toLowerCase()
   

96107551be011a5bef565b488eb7dddb4d6647eb
GuriZenit <israelreis17@gmail.com>
2021-07-04 21:48:31 -0300

    commands.js automatizado

--- commands/commands.js
+++ commands/commands.js
@@ -1,5 +1,12 @@
 module.exports.run = async (channel, tags, msg, self, client, args, db) => {
+  
   const value = await db.getAll()
+  
   console.log(value)
-  client.say(channel, value)
+  
+  var dados = '|';
+  for(values in value){
+    dados+= `${values}| `
+  }
+  client.say(channel,`Essa é a lista com todos os comandos: /me ${value}`)
 }

3b2739519349105faa2f188f72f3533fa87264e7
GuriZenit <israelreis17@gmail.com>
2021-07-06 12:43:55 -0300

    Checkpoint - 🐹

--- express.js
+++ express.js
@@ -1,8 +1,9 @@
 {
+  const login = require("./login.js")
   const express = require('express')
   const app = express()
   app.get('/', (req, res) => {
-    res.send(`Bot online at https://www.twitch.tv/popout/gurizenit/chat`)
+    res.send(`Bot online at https://www.twitch.tv/popout/${login.channels[0]}/chat`)
   });
   app.listen(3000, () => {
     console.log('Server online')

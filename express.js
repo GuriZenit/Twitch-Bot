1d223c48a020bede97d2614a3ee4485c451e2662
gurizenit <77638636+GuriZenit@users.noreply.github.com>
2021-07-06 15:58:31 +0000

    Checkpoint - 🐺

--- express.js
+++ express.js
@@ -3,7 +3,8 @@
   const express = require('express')
   const app = express()
   app.get('/', (req, res) => {
-    res.send(`Bot online at https://www.twitch.tv/popout/${login.channels[0]}/chat`)
+  	let ch = login.channels[0].slice(1)
+    res.send(`Bot online at https://www.twitch.tv/popout/${ch}/chat`)
   });
   app.listen(3000, () => {
     console.log('Server online')
